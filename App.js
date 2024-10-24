6aa7eadc568cf6dd684b3c8d3fe05cb31a5c56da
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-24 16:34:38 +0200

    Refactor App.js to use @shopify/react-native-skia for rendering canvas

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  const { width, height } = useWindowDimensions();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <Canvas style={{ width, height }}></Canvas>;
+};
+export default App;
