7b37d7612e64b15f646e90ed6cd2ce872c4f1959
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-24 16:47:50 +0200

    Refactor App.js to use @shopify/react-native-skia for rendering canvas and add Flappy Bird assets

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { Canvas } from "@shopify/react-native-skia";
+import { Canvas, useImage, Image } from "@shopify/react-native-skia";
 import { useWindowDimensions } from "react-native";
 
 const App = () => {
   const { width, height } = useWindowDimensions();
 
-  return <Canvas style={{ width, height }}></Canvas>;
+  const backgroundImage = useImage(
+    require("./assets/sprites/background-day.png")
+  );
+  const birdImage = useImage(
+    require("./assets/sprites/yellowbird-midflap.png")
+  );
+
+  return (
+    <Canvas style={{ width, height }}>
+      <Image
+        image={backgroundImage}
+        width={width}
+        height={height}
+        fit={"cover"}
+      />
+      <Image
+        image={birdImage}
+        x={width / 2}
+        y={height / 2}
+        width={34}
+        height={24}
+      />
+    </Canvas>
+  );
 };
 export default App;
