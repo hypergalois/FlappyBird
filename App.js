ab9fcff83bd6706d8c354848349d111309c0f7d3
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-26 17:11:11 +0200

    Adjust start game image position and size in App.js

--- App.js
+++ App.js
@@ -180,6 +180,18 @@ const App = () => {
           {/* Game over */}
         </Canvas>
       </GestureDetector>
+      <Pressable
+        onPress={startGame}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
     </GestureHandlerRootView>
   );
 };
